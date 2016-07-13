cbfda70dea39dfdff0007268fb245a738ad43b95
mark <wangjunfei@uml-tech.com>
2016-07-13 15:39:11 +0800

    更新

---
+++ CircleWaveAnimation/CircleWaveAnimation/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  VehicleTest
+//
+//  Created by apple on 20/6/16.
+//  Copyright © 2016年 mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
